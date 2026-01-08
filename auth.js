33a6383f7cb8c3845066722133029798ea28d9a2
DT DEV ID <153781142+dhitoid@users.noreply.github.com>
2026-01-09 04:17:22 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,13 +1,13 @@
 const SUPABASE_URL = 'https://lgkbrgwgdgqfhidjivcr.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxna2JyZ3dnZGdxZmhpZGppdmNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4OTIxMzAsImV4cCI6MjA4MzQ2ODEzMH0.ejJGDTlt_rt69Aa_egmLi8X3mqIMUCaYGzCsXxhgOKY'
 
-const supabase = supabaseJs.createClient(
+const supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 )
 
 async function loginGoogle() {
-  const { error } = await supabase.auth.signInWithOAuth({
+  const { error } = await supabaseClient.auth.signInWithOAuth({
     provider: 'google',
     options: {
       redirectTo: window.location.origin
@@ -18,11 +18,12 @@ async function loginGoogle() {
 }
 
 async function logout() {
-  await supabase.auth.signOut()
+  await supabaseClient.auth.signOut()
   location.reload()
 }
 
 async function checkUser() {
-  const { data: { session } } = await supabase.auth.getSession()
+  const { data: { session } } =
+    await supabaseClient.auth.getSession()
   return session?.user || null
 }
